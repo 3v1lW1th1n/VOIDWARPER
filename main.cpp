5987f09eb4864589cf1e2218a74ff26e926e28b9
z\x00^@%00U+2400 <54753063+FULLSHADE@users.noreply.github.com>
2020-05-01 18:56:31 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,52 @@
 #include <windows.h>
-#include <iostream.h>
+#include <iostream>
+#include <tlhelp32.h>
 
+//-----------------------------------------------------------
+// Get process PID from provided name
+//-----------------------------------------------------------
+
+int getProcessPid(){
+
+    HANDLE pidSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+    PROCESSENTRY32 p32;
+    p32.dwSize = sizeof(PROCESSENTRY32);
+
+    if(pidSnapshot == INVALID_HANDLE_VALUE){
+        std::cout << "[!] Failed to get process PID" << GetLastError() << std::endl;
+    }
+
+    int pid = 0;
+    char targetProcess[] = "notepad.exe";
+
+    while(Process32Next(pidSnapshot, &p32)){
+
+        if(strcmp(p32.szExeFile, targetProcess) == 0) {
+            pid = p32.th32ProcessID;
+            std::cout << "[+] " << targetProcess << "- PID - " << pid;
+        } else {
+            std::cout << "[!] Failed to get associated PID\n";
+        }
+    }
+    return pid;
+    CloseHandle(pidSnapshot);
+}
+
+//-----------------------------------------------------------
+// Inject DLL into target process with LoadLibrary technique
+//-----------------------------------------------------------
 void loadlibraryTechnique(){
     // loadLibrary (classic DLL injection technique)
     // provide PID to inject (for an update provide process snapshot walking)
     int dwProcessId = 4504;
-    if(OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessId)){
+    HANDLE procOpen;
+    if(procOpen = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessId)){
         std::cout << "[+] Opened access to the process : " << dwProcessId;
     } else {
         std::cout << "[!] Failed to open access to the process : " << dwProcessId << GetLastError();
     }
-    if(VirtualAllocEx(oProc, NULL, <soemthing> ,VIRTUAL_MEM ,PAGE_READWRITE)){
+    LPVOID dllSize = sizeof();
+    if(VirtualAllocEx(procOpen, NULL, dllSize ,MEM_COMMIT ,PAGE_READWRITE)){
         std::cout << "[+] Memory allocated : " << dwProcessId;
     } else {
         std::cout << "[!] Failed to allocate memory : " << GetLastError();
@@ -19,6 +55,14 @@ void loadlibraryTechnique(){
     //WriteProcessMemory();
     //CreateRemoteThread();
 
+
+}
+
+void usage(){
+
+    std::cout << "VOIDWARPER - DLL injection tool w/ support for multiple methods" << std::endl;
+    std::cout << "[1] LoadLibrary DLL injection";
+    std::cout << "Example : VOIDWARPER.exe <Process name> <DLL path to inject> <method>\n";
 
 }
 
